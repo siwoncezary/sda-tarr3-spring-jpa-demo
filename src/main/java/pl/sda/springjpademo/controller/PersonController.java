7e431665140102f1c71ac46e20ad94e73f276077
cs <siwon.cezary@gmail.com>
2020-07-26 11:41:05 +0200

    html

--- src/main/java/pl/sda/springjpademo/controller/PersonController.java
+++ src/main/java/pl/sda/springjpademo/controller/PersonController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import pl.sda.springjpademo.entity.Person;
 import pl.sda.springjpademo.service.PersonService;
-import pl.sda.springjpademo.service.PersonServiceJpa;
 
 @Controller
 public class PersonController {
@@ -16,7 +15,7 @@ public class PersonController {
         this.personService = personService;
     }
 
-    @GetMapping("/")
+    @GetMapping("/person")
     public String home(){
         Person person = Person.builder().email("marek@op.pl").name("marek").phone("555555666").build();
         personService.create(person);
