c61bec304d118f5772d7cf753bd79877ad98845b
cs <siwon.cezary@gmail.com>
2020-08-02 14:12:58 +0200

    person create form

--- src/main/java/pl/sda/springjpademo/controller/PersonController.java
+++ src/main/java/pl/sda/springjpademo/controller/PersonController.java
@@ -2,9 +2,14 @@ package pl.sda.springjpademo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.sda.springjpademo.entity.Person;
 import pl.sda.springjpademo.service.PersonService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class PersonController {
@@ -16,12 +21,21 @@ public class PersonController {
     }
 
     @GetMapping("/person")
-    public String home(){
-        Person person = Person.builder().email("marek@op.pl").name("marek").phone("555555666").build();
-        personService.create(person);
-        personService.findAll().forEach(System.out::println);
-        personService.updatePhone(1, "456456778");
-        personService.findAll().forEach(System.out::println);
-        return "index";
+    public String personIndex(Model model){
+        List<Person> persons = personService.findAll().collect(Collectors.toList());
+        model.addAttribute("persons", persons);
+        return "persons";
     }
+
+    @GetMapping("/person/create")
+    public String personCreate(Person person){
+        return "personCreate";
+    }
+
+    @PostMapping("/person/create")
+    public String handlePersonCreate(Person person){
+        personService.create(person);
+        return "redirect:/person";
+    }
+
 }
--- src/main/resources/templates/exchanger.js
+++ src/main/resources/templates/exchanger.js
@@ -21,13 +21,29 @@ function init(){
             const table = JSON.parse(this.response);
             let rates = table[0].rates;
             const sourceCurrency = document.getElementById("source-currency");
-            const targetCurrency = document.getElementById("target-currency")
+            const targetCurrency = document.getElementById("target-currency");
             for(index in rates){
                 let item = rates[index];
-                sourceCurrency.innerHTML += `<option value=${index}>${item.currency}</option>`;
-                targetCurrency.innerHTML += `<option value=${index}>${item.currency}</option>`;
+                sourceCurrency.innerHTML += `<option value=${item.mid} name=${item.code}>${item.currency}</option>`;
+                targetCurrency.innerHTML += `<option value=${item.mid} name=${item.code}>${item.currency}</option>`;
             }
+            sourceCurrency.innerHTML += `<option value="1.00" name="PLN">złoty polski</option>`;
+            targetCurrency.innerHTML += `<option value="1.00" name="PLN">złoty polski</option>`;
+            //TODO wstawić datę aktualnych kursów do stopki na stronie z pola effectiveDate
+            document.getElementById("table-date").innerText = table[0].effectiveDate;
         }
     }
     ajax.send();
+}
+
+function calculate(){
+    const midSource = document.getElementById("source-currency").value;
+    const midTarget = document.getElementById("target-currency").value;
+    const amountSource = document.getElementById("source-amount").value;
+    const targetAmount = amountSource * midSource/midTarget;
+    const index = document.getElementById("target-currency").selectedIndex;
+    let code = document.getElementById("target-currency").options[index].getAttribute("name");
+
+    const target = Intl.NumberFormat("pl-PL", {style: "currency", currency: code}).format(targetAmount);
+    document.getElementById("target-amount").value = target;
 }
